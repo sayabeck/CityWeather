af2a0a7c4f0fdb341a626286e05489f8d7adccbe
sayabeck <sayabeck@mail.ru>
2022-05-09 02:32:43 +0600

    Initial Commit

---
+++ CityWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CityWeather
+//
+//  Created by mac mini on 5/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
